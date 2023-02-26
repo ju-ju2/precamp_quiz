71c52e83fa21227a7c5b97f4820a2cf7c57875fd
한주연 <hjy2859@naver.com>
2023-02-26 15:00:46 +0900

    gql의 deleteBoard에서 데이터 삭제하기

--- pages/07/products/index.js
+++ pages/07/products/index.js
@@ -1,13 +1,24 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import styled from "@emotion/styled";
 
 const FETCH_PRODUCTS = gql`
   query {
     fetchProducts {
+      _id
       seller
       name
       detail
       price
+    }
+  }
+`;
+
+const DELETE_PRODUCT = gql`
+  mutation typeSetting($productId: ID) {
+    deleteProduct(productId: $productId) {
+      _id
+      number
+      message
     }
   }
 `;
@@ -22,20 +33,35 @@ const Column = styled.div`
 
 export default function ProductsList() {
   const { data } = useQuery(FETCH_PRODUCTS);
+  console.log(data);
+  const [deleteProduct] = useMutation(DELETE_PRODUCT);
+
+  const onClickDelete = async (event) => {
+    console.log(event.target);
+    const result = await deleteProduct({
+      variables: {
+        productId: event.target.id,
+      },
+      refetchQueries: [{ query: FETCH_PRODUCTS }],
+    });
+  };
 
   return (
     <>
       {data?.fetchProducts.map((el) => (
-        <Row>
+        <Row key={el._id}>
           <Column>
             <input type="checkbox"></input>
           </Column>
+          <Column>{el._id}</Column>
           <Column>{el.seller}</Column>
           <Column>{el.name}</Column>
           <Column>{el.detail}</Column>
           <Column>{el.price}</Column>
           <Column>
-            <button>삭제하기</button>
+            <button id={el._id} onClick={onClickDelete}>
+              삭제하기
+            </button>
           </Column>
         </Row>
       ))}
