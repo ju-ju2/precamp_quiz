962e5471ac0771e3fe17a27912f954653735dddf
한주연 <hjy2859@naver.com>
2023-03-12 22:39:34 +0900

    글로벌 스타일 적용하기

--- pages/09/08-layout/index.js
+++ pages/09/08-layout/index.js
@@ -1,13 +1,16 @@
+import { globalStyle } from "@/src/commons/styles/globalStyle";
 import Banner from "@/src/components/commons/layout/banner";
 import BodyWrapper from "@/src/components/commons/layout/bodyWrapper";
 import Footer from "@/src/components/commons/layout/footer";
 import Header from "@/src/components/commons/layout/header";
 import Navigation from "@/src/components/commons/layout/navigation";
 import Sidebar from "@/src/components/commons/layout/sidebar";
+import { Global } from "@emotion/react";
 
 export default function LayoutPage(props) {
   return (
     <div>
+      <Global styles={globalStyle} />
       <Header />
       <Banner />
       <Navigation />
@@ -15,7 +18,6 @@ export default function LayoutPage(props) {
         <Sidebar />
         <div>{props.children || "바디영역"}</div>
       </BodyWrapper>
-
       <Footer />
     </div>
   );
---
+++ src/commons/styles/globalStyle.js
@@ -0,0 +1,8 @@
+import { css } from "@emotion/react";
+
+export const globalStyle = css`
+  * {
+    font-size: 30px;
+    box-sizing: border-box;
+  }
+`;
