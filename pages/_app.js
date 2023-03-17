57adf964304337e2ecfa3d8aa445f1895788f929
한주연 <hjy2859@naver.com>
2023-03-17 21:50:21 +0900

    아폴로 세팅

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,21 @@ import "../styles/myglobal.css";
 //   return <Component {...pageProps} />;
 // }
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
 
 export default function App({ Component, pageProps }) {
+  const upLoadLink = createUploadLink({
+    uri: "http://backendonline.codebootcamp.co.kr/graphql",
+  });
   const client = new ApolloClient({
     // uri: "http://practice.codebootcamp.co.kr/graphql",
-    uri: "http://backendonline.codebootcamp.co.kr/graphql",
-
+    link: ApolloLink.from([upLoadLink]),
     cache: new InMemoryCache(),
   });
 
