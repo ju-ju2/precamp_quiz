25a9542bcaf84b60eea96170054ec8d159e920d0
한주연 <hjy2859@naver.com>
2023-03-17 23:48:36 +0900

    파일 제한 조건문 걸기

--- pages/11/index.js
+++ pages/11/index.js
@@ -47,6 +47,23 @@ export default function ImageUploadMutation() {
   const onChangeFile = async (event) => {
     const file = event.target.files?.[0];
 
+    if (!file?.size) {
+      Modal.error({ content: "파일이 없습니다" });
+      return;
+    }
+    if (file.size > 5 * 1024 * 1024) {
+      Modal.error({ content: "파일 크기가 너무 큽니다." });
+      return;
+    }
+    if (
+      !file.type.includes("jpeg") &&
+      !file.type.includes("jpg") &&
+      !file.type.includes("png")
+    ) {
+      Modal.error({ content: "지원하지않는 확장자입니다." });
+      return;
+    }
+
     try {
       const result = await uploadFile({
         variables: {
