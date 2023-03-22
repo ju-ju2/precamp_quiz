0a5bce5b67734fc5094a07c686e71427911b16e1
한주연 <hjy2859@naver.com>
2023-03-22 23:40:54 +0900

    loginUser 뮤테이션 로직

---
+++ pages/14/login.js
@@ -0,0 +1,42 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState } from "react";
+
+const LOGIN_USER = gql`
+  mutation groupName($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      accessToken
+    }
+  }
+`;
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginUser] = useMutation(LOGIN_USER);
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+  const onClickLogin = async () => {
+    const result = await loginUser({
+      variables: {
+        email,
+        password,
+      },
+    });
+    console.log(result);
+  };
+  return (
+    <>
+      이메일 : <input onChange={onChangeEmail} type="text" />
+      <br />
+      비밀번호 : <input onChange={onChangePassword} type="password" />
+      <br />
+      <button onClick={onClickLogin}>로그인</button>
+    </>
+  );
+}
---
+++ pages/14/loginSuccess.js
@@ -0,0 +1,7 @@
+export default function LoginSuccessPage() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
