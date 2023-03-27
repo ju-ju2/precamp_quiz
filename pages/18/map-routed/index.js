2b200b5652a08a05451967c306232f2d346ea14d
한주연 <hjy2859@naver.com>
2023-03-28 01:16:22 +0900

    페이지 이동해도 지도 불러올 수 있도록 함수로직 수정/ CSR Link 태그

--- pages/18/map-routed/index.js
+++ pages/18/map-routed/index.js
@@ -3,23 +3,28 @@ import { useEffect } from "react";
 
 export default function MapRoutedPage() {
   useEffect(() => {
-    const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-    const options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
-    };
+    // JSX문법에서 스크립트 태그를 만들어주는 과정
+    // 리턴의 결과가 화면에 먼저 그려지고 useEffect를 통해 데이터를 후에 받아오기 위함
+    const script = document.createElement("script");
+    script.src =
+      "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=9f7ec2c230c4669c85bacfe30e89b7e3";
+    document.head.appendChild(script);
 
-    const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    script.onload = () => {
+      window.kakao.maps.load(function () {
+        const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+        const options = {
+          //지도를 생성할 때 필요한 기본 옵션
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+          level: 3, //지도의 레벨(확대, 축소 정도)
+        };
+
+        const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+      });
+    };
   }, []);
   return (
     <>
-      <Head>
-        <script
-          type="text/javascript"
-          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=9f7ec2c230c4669c85bacfe30e89b7e3"
-        ></script>
-      </Head>
       <div id="map" style={{ width: "500px", height: "400px" }}></div>
     </>
   );
--- pages/18/map-routing/index.js
+++ pages/18/map-routing/index.js
@@ -1,13 +1,17 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function MapRoutingPage() {
-  const router = useRouter();
-  const onClickMoveToMap = () => {
-    router.push("./map-routed");
-  };
+  //   const router = useRouter();
+  //   const onClickMoveToMap = () => {
+  //     router.push("./map-routed");
+  //   };
   return (
     <>
-      <button onClick={onClickMoveToMap}>지도로 이동하기</button>
+      {/* <button onClick={onClickMoveToMap}>지도로 이동하기</button> */}
+      <Link href="./map-routed">
+        <a>지도로 이동하기</a>
+      </Link>
     </>
   );
 }
